040b6f3187695dd4a38ba4248013e982edf647c9
mustafab11 <mustafab11@yahoo.com>
2022-02-16 21:38:55 +0200

    cukes runner wip

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/zerobank/stepdefnitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@smoke"
 )
 
 public class CukesRunner {
